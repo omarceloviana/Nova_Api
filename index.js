fc46fdb775bfe9ad7584df2be67f87f741d06dd6
Marcelo Viana <marcelo.viana3407@gmail.com>
2020-12-04 16:06:08 -0300

    Criação das rotas

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const readFile = () => {
 
 const WriteFile = content => {
     const updateFile = JSON.stringify(content);
-    writeFileSync("./Data/itens.json",updateFile,"utf-8");
+    fs.writeFileSync("./Data/itens.json",updateFile,"utf-8");
 }
 
 router.get('/',function(req,res){
@@ -21,16 +21,49 @@ router.get('/',function(req,res){
 
 router.post('/',function(req,res){
     const currentContent = readFile();
-    const {id,nome,endereço,cep,data_de_nascimento} = req.body;
-    const ident = Match.random().toString(32);
-    currentContent.push({id,nome,endereço,cep,data_de_nascimento});
+    const nome = req.body.nome;
+    const endereço = req.body.endereço;
+    const cep = req.body.cep;
+    const nascimento = req.body.nascimento;
+
+    const id = Math.random().toString(32).substr(2.9);
+    currentContent.push({nome,endereço,cep,nascimento});
     writeFile(currentContent);
     res.send(currentContent);
 })
 
 router.put('/:id',function(req,res){
+    const {id} = req.params;
+    const nome = req.body.nome;
+    const endereço = req.body.endereço;
+    const cep = req.body.cep;
+    const nascimento = req.body.nascimento;
+   
+    const currentContent = readFile();
+    const selectedItem = currentContent.findIndex ((item) => item.id === id);
 
+    const {id: cId, nome: cNome, cep: cCep, nascimento: cNascimento} = currentContent[selectedItem];
+    
+    const newObject = {
+        id: cId,
+        nome: cNome ? nome: cNome,
+        cep: cCep ? cep: cCep,
+        nascimento: cNascimento ? nascimento: cNascimento,
+    };
+
+    currentContent[selectedItem] = newObject;
+    writeFile(currentContent);
+    res.send(newObject);
 });
+
+router.delete('/:id',function(req,res){
+    const {id} = req.params;
+    const currentContent = readFile();
+    const selectedItem = currentContent.findIndex ((item) => item.id === id);
+    currentContent.splice(selectedItem, 1);
+    writeFile(currentContent);
+    res.send(true);
+})
 
 app.use(router);
 app.listen(8080,function(){
